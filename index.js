121133e0f8dbca71fba0e6b4256c6c3f335de289
Imprakashsah <prakash@xgenmedia.com>
2022-07-15 17:09:53 +0530

    added new modules

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
 
+app.use(express.json());
 // app.get();
 // app.post();
 // app.put();
@@ -14,6 +16,8 @@ const courses = [
     {id: 3, name : 'C programming'},
 ]
 //routing starts
+
+//Get method............
 app.get('/', (req,res)  =>{
         res.send('Hello World');
 });
@@ -25,6 +29,26 @@ app.get('/api/course/:id', (req,res)  =>{
     const course = courses.find(c => c.id === parseInt(req.params.id));
     if(!course) res.status(404).send('The course is not found');
     res.send(course); 
+});
+//post method......................
+app.post('/api/course', (req,res)  =>{
+    const schema ={
+        name: Joi.string().min(3).required()
+
+    };
+    const result = Joi.ValidationError(res.body, scheme);
+    console.log(result);
+    //validation
+    if (!req.body.name || req.body.name.length < 3){
+        res.status(404).send('Name is required')
+        return; 
+    }
+   const course = {
+    id: courses.length + 1,
+    name: req.body.name
+   };
+   courses.push(course);
+   res.send(course);
 });
 
 //configuring port number
